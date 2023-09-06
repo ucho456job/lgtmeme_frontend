feb81abd24717aed88ba60488c1416b0c01afc19
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-09-06 22:47:07 +0900

    Initialize prisma and add sample api (#2)

---
+++ src/app/api/images/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const initPrisma = async () => {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("Failed: DB connection")
+  }
+}
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await initPrisma();
+    const images = await prisma.image.findMany();
+    return NextResponse.json({ message: "Success", images }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Failed", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
