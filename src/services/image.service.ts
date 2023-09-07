eba341f5a4cb0d52a78ac0fb3d93caa4df9dc37c
ucho456job <ucho456job@gmail.com>
2023-09-07 19:29:57 +0900

    Display image title

---
+++ src/services/image.service.ts
@@ -0,0 +1,53 @@
+import { GET_IMAGES_ENDPOINT } from "@/constants/endpoints";
+import { RequestOptions } from "https";
+
+type Method = 'GET'
+
+type Cache = 'no-store'
+
+type Config = {
+  method: Method
+  cache: Cache;
+  headers: {
+    "Content-Type": string;
+  }
+}
+
+type ImageType = {
+  id: string;
+  title: string;
+  categoryId: string;
+  tags: string[];
+  path: string;
+  createdAt: Date;
+}
+
+export class ImageService {
+  private baseUrl: string;
+
+  constructor() {
+    this.baseUrl = process.env.APP_URL
+  }
+
+  private createConfig(method: Method, cache: Cache): Config {
+    return {
+      method,
+      cache,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+
+  private async sendRequest<T>(path: string, config: Config) {
+    const res = await fetch(path, config)
+    const data: T = await res.json();
+    return data
+  }
+
+  async fetchImages(): Promise<ImageType[]> {
+    const config = this.createConfig('GET', 'no-store');
+    const res = await this.sendRequest<{ images: ImageType[] }>(this.baseUrl + GET_IMAGES_ENDPOINT, config)
+    return res.images
+  }
+}
