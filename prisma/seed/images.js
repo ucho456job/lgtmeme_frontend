03b3041bdb89c54ee8b0e049254a83d75aa2dd90
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-09-08 20:42:42 +0900

    Add docker compose and create db for development (#9)

---
+++ prisma/seed/images.js
@@ -0,0 +1,11 @@
+const imagesData = [
+  {
+    id: "1b2fa70d-8416-42c3-a789-9",
+    title: "image A",
+    categoryId: "ee47388b-a891-4772-83c0-b2",
+    tags: [],
+    path: "image path",
+  },
+];
+
+module.exports = { imagesData };
