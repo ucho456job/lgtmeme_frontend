edb55577aa12a6c501a674298fc1a615e7334f26
ucho456job <ucho456job@gmail.com>
2023-09-08 16:58:49 +0900

    Delete vercel.json and fix build script

--- vercel-build.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  execSync('npx prisma generate', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Prisma generate failed:', error);
-  process.exit(1);
-}
