c56cbe480415e20fd3c55ad5f5cd11ad0ddc0cde
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-09-10 01:54:53 +0900

    Modify button (#14)

--- src/components/atoms/Button/Button.stories.ts
+++ src/components/atoms/Button/Button.stories.ts
@@ -1,45 +1,21 @@
-// import { Button } from './index';
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@/components/atoms/Button";
+import Button from "@/components/atoms/Button";
 
 const meta = {
-  title: "Example/Button",
+  title: "Button",
   component: Button,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: "Button",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
+    variant: "contained",
+    color: "black",
+    size: "md",
+    disabled: false,
+    children: "Button",
+    onClick: () => alert("Click!"),
   },
 };
