238e066ac163cc22d9e25e60686af751a6a451cf
ucho456job <ucho456job@gmail.com>
2023-09-14 10:14:03 +0900

    Modify header

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
-import { cva } from "@@/styled-system/css";
+import { css } from "@@/styled-system/css";
 
 const Header = () => {
   return (
-    <header className={headerRecipe()}>
+    <header className={headerCss}>
       <Link href="/">
-        <h1 className={h1Recipe()}>LGTMeme</h1>
+        <h1 className={h1Css}>LGTMeme</h1>
       </Link>
     </header>
   );
 };
 
-const headerRecipe = cva({
-  base: { bgColor: "BLACK", color: "WHITE", maxWidth: "100vw", height: "60px" },
-});
-
-const h1Recipe = cva({
-  base: { fontSize: "4xl", textAlign: "center", _hover: { opacity: 0.8 } },
-});
+const headerCss = css({ bgColor: "BLACK", color: "WHITE", maxWidth: "100vw", height: "60px" });
+const h1Css = css({ fontSize: "4xl", textAlign: "center", _hover: { opacity: 0.8 } });
 
 export default Header;
