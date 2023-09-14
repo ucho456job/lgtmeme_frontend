c6714f0e3fadc1f282e2c9d106b545e544f7e171
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-09-15 01:37:28 +0900

    Fix vercel ignore build step script (#32)

--- shells/vercel_ignore_build_step.sh
+++ shells/vercel_ignore_build_step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "dev" ]]; then
   echo "Build can proceed"
   exit 1;
 else
