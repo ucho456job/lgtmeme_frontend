2d2dc465bfa8263703917ca32def74ea81934a40
ucho456job <ucho456job@gmail.com>
2023-09-20 22:48:12 +0900

    save

--- src/app/add/ImageEditor.tsx
+++ src/app/add/ImageEditor.tsx
@@ -7,16 +7,14 @@ type TextStyle = {
   left: number;
   top: number;
   color: string;
-  textStroke: string;
-  WebkitTextStroke: string;
-  textBorderColor: string;
   fontSize: number;
   width: number;
   height: number;
+  lineHeight: string;
   fontFamily: string;
 };
 
-type SizeMapKey = "12" | "24" | "36" | "48" | "60" | "72" | "84" | "96";
+type SizeMapKey = 12 | 24 | 36 | 48 | 60 | 72 | 84 | 96;
 
 type SizeMap = Map<
   SizeMapKey,
@@ -24,21 +22,20 @@ type SizeMap = Map<
     size: number;
     width: number;
     height: number;
+    diff: number;
   }
 >;
 
 const ImageEditor = () => {
   const [image, setImage] = useState<string | null>(null);
   const [textStyle, setTextStyle] = useState<TextStyle>({
-    left: 100,
-    top: 125,
-    color: "#ffffff",
-    textStroke: "1px #000000",
-    WebkitTextStroke: "1px #000000",
-    textBorderColor: "#000000",
-    fontSize: 36,
-    width: 102,
-    height: 56,
+    left: 66,
+    top: 120,
+    color: "#000000",
+    fontSize: 60,
+    width: 169,
+    height: 60,
+    lineHeight: "60px",
     fontFamily: "Arial",
   });
   const [isDragging, setIsDragging] = useState(false);
@@ -79,32 +76,23 @@ const ImageEditor = () => {
   };
 
   const sizeMap: SizeMap = new Map([
-    ["12", { size: 12, width: 35, height: 20 }],
-    ["24", { size: 24, width: 69, height: 38 }],
-    ["36", { size: 36, width: 102, height: 56 }],
-    ["48", { size: 48, width: 135, height: 74 }],
-    ["60", { size: 60, width: 169, height: 92 }],
-    ["72", { size: 72, width: 202, height: 110 }],
-    ["84", { size: 84, width: 235, height: 128 }],
-    ["96", { size: 96, width: 269, height: 146 }],
+    [36, { size: 36, width: 102, height: 36, diff: 30 }],
+    [60, { size: 60, width: 169, height: 60, diff: 50 }],
+    [84, { size: 84, width: 235, height: 84, diff: 71 }],
   ]);
   const handleTextSizeChange = (e: React.ChangeEvent<HTMLSelectElement>, sizeMap: SizeMap) => {
-    const map = sizeMap.get(e.target.value as SizeMapKey)!;
-    setTextStyle((prev) => ({ ...prev, fontSize: map.size, width: map.width, height: map.height }));
+    const map = sizeMap.get(Number(e.target.value) as SizeMapKey)!;
+    setTextStyle((prev) => ({
+      ...prev,
+      fontSize: map.size,
+      width: map.width,
+      height: map.height,
+      lineHeight: `${map.size}px`,
+    }));
   };
 
   const handleTextColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTextStyle((prev) => ({ ...prev, color: e.target.value }));
-  };
-
-  const handleTextBorderColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newColor = e.target.value;
-    setTextStyle((prev) => ({
-      ...prev,
-      textStroke: `1px ${newColor}`,
-      WebkitTextStroke: `1px ${newColor}`,
-      textBorderColor: newColor,
-    }));
   };
 
   const handleFontFamilyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -137,23 +125,20 @@ const ImageEditor = () => {
     setIsDragging(false);
   };
 
-  const downloadWebPImage = () => {
-    if (canvasRef.current) {
+  const downloadWebPImage = (size: SizeMapKey, sizeMap: SizeMap) => {
+    const diff = sizeMap.get(size)?.diff;
+    if (canvasRef.current && diff) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
       if (!ctx) return;
       ctx.fillStyle = textStyle.color;
       ctx.font = `${textStyle.fontSize}px ${textStyle.fontFamily}`;
-      ctx.strokeStyle = textStyle.textBorderColor;
-      ctx.lineWidth = 3;
       const text = "LGTM";
       const textX = textStyle.left;
-      const textY = textStyle.top;
-      ctx.strokeText(text, textX, textY);
+      const textY = textStyle.top + diff;
       ctx.fillText(text, textX, textY);
 
       const dataURL = canvas.toDataURL("image/webp");
-
       const a = document.createElement("a");
       a.href = dataURL;
       a.download = "image.webp";
@@ -171,14 +156,9 @@ const ImageEditor = () => {
           value={textStyle.fontSize}
           onChange={(e) => handleTextSizeChange(e, sizeMap)}
         >
-          <option value="12">12</option>
-          <option value="24">24</option>
-          <option value="36">36</option>
-          <option value="48">48</option>
-          <option value="60">60</option>
-          <option value="72">72</option>
-          <option value="84">84</option>
-          <option value="96">96</option>
+          <option value="36">Small</option>
+          <option value="60">Medium</option>
+          <option value="84">Large</option>
         </select>
       </div>
       <div>
@@ -190,15 +170,7 @@ const ImageEditor = () => {
           onChange={(e) => handleTextColorChange(e)}
         />
       </div>
-      <div>
-        <label htmlFor="textBorderColor">Text Border Color: </label>
-        <input
-          type="color"
-          id="textBorderColor"
-          value={textStyle.textBorderColor}
-          onChange={(e) => handleTextBorderColorChange(e)}
-        />
-      </div>
+
       <div>
         <label htmlFor="fontFamily">Font Family: </label>
         <select
@@ -225,7 +197,9 @@ const ImageEditor = () => {
           </div>
         )}
       </div>
-      <button onClick={downloadWebPImage}>Download WebP</button>
+      <button onClick={() => downloadWebPImage(textStyle.fontSize as SizeMapKey, sizeMap)}>
+        Download WebP
+      </button>
     </div>
   );
 };
