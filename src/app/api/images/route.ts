31dc76d9b906f339a77fdeb46336ae5ebc65cf99
ucho456job <ucho456job@gmail.com>
2023-09-21 23:28:50 +0900

    Fix type error

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { ActiveTabId } from "@/components/organisms/ImageGallery";
+import { ActiveTabId } from "@/app/ImageGallery";
 import prisma from "@/utils/client";
 
 const LIMIT = 9;
