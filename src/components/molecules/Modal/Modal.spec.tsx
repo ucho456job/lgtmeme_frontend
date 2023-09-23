a20cd482a9c5b6b30e9987ee8a0db74206149722
ucho456job <ucho456job@gmail.com>
2023-09-23 22:47:36 +0900

    Add modal

---
+++ src/components/molecules/Modal/Modal.spec.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Modal from "@/components/molecules/Modal/Modal";
+
+describe("Modal", () => {
+  let onClickMock: jest.Mock<any, any, any>;
+  const messageData = "Test message";
+  beforeEach(() => {
+    onClickMock = jest.fn();
+  });
+  afterEach(() => {
+    onClickMock.mockReset();
+  });
+  describe("Render tests", () => {
+    test("Renders with default props", () => {
+      render(<Modal message={messageData} onClick={onClickMock} />);
+      const message = screen.getByText(messageData);
+      const button = screen.getByRole("button", { name: "Close" });
+      expect(message).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+    });
+  });
+  describe("Event tests", () => {
+    test("Calls onClick", async () => {
+      render(<Modal message={messageData} onClick={onClickMock} />);
+      const button = screen.getByRole("button", { name: "Close" });
+      await userEvent.click(button);
+      expect(onClickMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
---
+++ src/components/molecules/Modal/Modal.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Modal from "@/components/molecules/Modal/Modal";
+
+const meta = {
+  title: "molecules/Modal",
+  component: Modal,
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    message: "Success! Create LGTM image!",
+  },
+};
