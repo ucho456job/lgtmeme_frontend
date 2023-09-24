7364b060c1e15a060cfee21ce1fcc5c9c85be488
ucho456job <ucho456job@gmail.com>
2023-09-24 18:22:47 +0900

    Initialize pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Favicon from "/public/favicon.ico";
 import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
 import "@/styles/globals.css";
@@ -10,7 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "LGTMeme",
   description:
-    "LGTMemeはシンプルでユーモアのあるLGTM（Looks Good To Me）画像作成サービスです。ネットミームで人気のある画像をベースにユニークなLGTM画像を作成・提供します。コミカルな要素を取り入れ、単調なコードレビューや承認プロセスを楽しいものにします。",
+    "LGTMemeはシンプルなLGTM（Looks Good To Me）画像作成サービスです。お気に入りの画像に素早くLGTMの文字を挿入してMarkdownをコピーできます。単調なコードレビューや承認プロセスにユーモアのあるLGTM画像を使って、華やかにしましょう。",
+  manifest: "/manifest.webmanifest",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
