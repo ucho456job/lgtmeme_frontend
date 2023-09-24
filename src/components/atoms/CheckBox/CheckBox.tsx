a81f00fff99f074be50347280d29375832b042f6
ucho456job <ucho456job@gmail.com>
2023-09-24 12:19:23 +0900

    Remove CheckBox.css

--- src/components/atoms/CheckBox/CheckBox.tsx
+++ src/components/atoms/CheckBox/CheckBox.tsx
@@ -1,4 +1,4 @@
-import "./CheckBox.css";
+import { css } from "@@/styled-system/css";
 
 type Props = {
   css?: string;
@@ -11,14 +11,31 @@ const CheckBox = ({ css, label, checked, onChange }: Props) => {
   const handleChange = () => onChange();
   return (
     <div className={css}>
-      <fieldset className="checkbox-002">
-        <label>
-          <input type="checkbox" name="checkbox-002" checked={checked} onChange={handleChange} />
-          {label}
-        </label>
-      </fieldset>
+      <input
+        className={inputCss}
+        type="checkbox"
+        id="check"
+        checked={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="check" className={a}>
+        <span className={checkCss}>{checked ? "✓" : ""}</span>
+        <div className={labelCss}>{label}</div>
+      </label>
     </div>
   );
 };
+
+const inputCss = css({ display: "none" });
+const a = css({ height: "22px", display: "flex" });
+const checkCss = css({
+  display: "inline-block",
+  width: "22px",
+  height: "22px",
+  border: "1px solid #cccccc",
+  borderRadius: "sm",
+  cursor: "pointer",
+});
+const labelCss = css({ height: "22px", marginLeft: "2", cursor: "pointer" });
 
 export default CheckBox;
