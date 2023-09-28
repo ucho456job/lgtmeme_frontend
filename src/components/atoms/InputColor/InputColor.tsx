6cfc9317022a88fb7f4c83c56109b18533b08997
ucho456job <ucho456job@gmail.com>
2023-09-28 18:27:36 +0900

    Modify color input

--- src/components/atoms/InputColor/InputColor.tsx
+++ src/components/atoms/InputColor/InputColor.tsx
@@ -20,30 +20,36 @@ const ColorInput = ({ css, value, onChange }: Props) => {
         onChange={handleChange}
       />
       <datalist id="datalist">
-        <option value="#ffffff"></option>
-        <option value="#f43f5e"></option>
-        <option value="#ec4899"></option>
-        <option value="#d946ef"></option>
-        <option value="#a855f7"></option>
-        <option value="#8b5cf6"></option>
-        <option value="#6366f1"></option>
-        <option value="#3b82f6"></option>
-        <option value="#0ea5e9"></option>
-        <option value="#06b6d4"></option>
-        <option value="#14b8a6"></option>
-        <option value="#10b981"></option>
-        <option value="#22c55e"></option>
-        <option value="#84cc16"></option>
-        <option value="#eab308"></option>
-        <option value="#f59e0b"></option>
-        <option value="#f97316"></option>
-        <option value="#ef4444"></option>
-        <option value="#737373"></option>
-        <option value="#78716c"></option>
-        <option value="#71717a"></option>
-        <option value="#6b7280"></option>
-        <option value="#64748b"></option>
-        <option value="#000000"></option>
+        <option value="#ffffff" />
+        {/** White */}
+        <option value="#f43f5e" />
+        {/** Rose */}
+        <option value="#ec4899" />
+        {/** Pink */}
+        <option value="#a855f7" />
+        {/** Purple */}
+        <option value="#3730a3" />
+        {/** Indigo */}
+        <option value="#3b82f6" />
+        {/** Blue */}
+        <option value="#0ea5e9" />
+        {/** Light blue */}
+        <option value="#14b8a6" />
+        {/** Teal */}
+        <option value="#22c55e" />
+        {/** Green */}
+        <option value="#a3e635" />
+        {/** Light green */}
+        <option value="#fde047" />
+        {/** Yellow */}
+        <option value="#f97316" />
+        {/** Orange */}
+        <option value="#ef4444" />
+        {/** Red */}
+        <option value="#64748b" />
+        {/** Gray */}
+        <option value="#000000" />
+        {/** Black */}
       </datalist>
     </div>
   );
