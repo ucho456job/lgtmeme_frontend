2632628e02abdee222ad63a2676b05ad7a9b34c0
ucho456job <ucho456job@gmail.com>
2023-10-06 11:33:58 +0900

    Fix image card test

--- src/components/molecules/ImageCard/ImageCard.spec.tsx
+++ src/components/molecules/ImageCard/ImageCard.spec.tsx
@@ -3,48 +3,73 @@ import userEvent from "@testing-library/user-event";
 import ImageCard from "@/components/molecules/ImageCard/ImageCard";
 
 describe("ImageCard", () => {
-  const imageData = {
+  const imageMock = {
     id: "1",
     url: "https://placehold.jp/300x300.png",
+    reported: false,
   };
   let onClickCopyMock: jest.Mock<any, any, any>;
   let onClickFavoriteMock: jest.Mock<any, any, any>;
+  let onClickReportMock: jest.Mock<any, any, any>;
   beforeEach(() => {
     onClickCopyMock = jest.fn();
     onClickFavoriteMock = jest.fn();
+    onClickReportMock = jest.fn();
   });
   afterEach(() => {
     onClickCopyMock.mockReset();
     onClickFavoriteMock.mockReset();
+    onClickReportMock.mockReset();
   });
   describe("Render tests", () => {
     test("Renders with default props", () => {
       render(
         <ImageCard
-          image={imageData}
+          image={imageMock}
           isFavorite={false}
           onClickCopy={onClickCopyMock}
           onClickFavorite={onClickFavoriteMock}
+          onClickReport={onClickReportMock}
         />,
       );
       const image = screen.getByAltText("LGTM");
       const copyButton = screen.getAllByRole("button")[0];
       const favoriteButton = screen.getAllByRole("button")[1];
-      // const reportButton = screen.getAllByRole("button")[2];
+      const reportButton = screen.getAllByRole("button")[2];
       expect(image).toBeInTheDocument();
       expect(copyButton).toBeInTheDocument();
       expect(favoriteButton).toBeInTheDocument();
-      // expect(reportButton).toBeInTheDocument();
+      expect(reportButton).toBeInTheDocument();
+    });
+    test("Renders with default props", () => {
+      render(
+        <ImageCard
+          image={imageMock}
+          isFavorite={false}
+          onClickCopy={onClickCopyMock}
+          onClickFavorite={onClickFavoriteMock}
+          onClickReport={onClickReportMock}
+        />,
+      );
+      const image = screen.getByAltText("LGTM");
+      const copyButton = screen.getAllByRole("button")[0];
+      const favoriteButton = screen.getAllByRole("button")[1];
+      const reportButton = screen.getAllByRole("button")[2];
+      expect(image).toBeInTheDocument();
+      expect(copyButton).toBeInTheDocument();
+      expect(favoriteButton).toBeInTheDocument();
+      expect(reportButton).toBeInTheDocument();
     });
   });
   describe("Event tests", () => {
     test("Calls onClickCopyMock", async () => {
       render(
         <ImageCard
-          image={imageData}
+          image={imageMock}
           isFavorite={false}
           onClickCopy={onClickCopyMock}
           onClickFavorite={onClickFavoriteMock}
+          onClickReport={onClickReportMock}
         />,
       );
       const copyButton = screen.getAllByRole("button")[0];
@@ -54,15 +79,30 @@ describe("ImageCard", () => {
     test("Calls onClickFavoriteMock", async () => {
       render(
         <ImageCard
-          image={imageData}
+          image={imageMock}
           isFavorite={false}
           onClickCopy={onClickCopyMock}
           onClickFavorite={onClickFavoriteMock}
+          onClickReport={onClickReportMock}
         />,
       );
       const favoriteButton = screen.getAllByRole("button")[1];
       await userEvent.click(favoriteButton);
       expect(onClickFavoriteMock).toHaveBeenCalledTimes(1);
+    });
+    test("Calls onClickReportMock", async () => {
+      render(
+        <ImageCard
+          image={imageMock}
+          isFavorite={false}
+          onClickCopy={onClickCopyMock}
+          onClickFavorite={onClickFavoriteMock}
+          onClickReport={onClickReportMock}
+        />,
+      );
+      const reportButton = screen.getAllByRole("button")[2];
+      await userEvent.click(reportButton);
+      expect(onClickReportMock).toHaveBeenCalledTimes(1);
     });
   });
 });
