4b206734388ed05bf6b9dfb9386f253db497c5cf
Yuto Araki <114855385+ucho456job@users.noreply.github.com>
2023-10-13 23:04:00 +0900

    Fix storybook (#117)

--- src/components/molecules/ImageCard/ImageCard.stories.tsx
+++ src/components/molecules/ImageCard/ImageCard.stories.tsx
@@ -15,44 +15,11 @@ const commonArgs = {
   onClickFavorite: () => alert("Click favorite button!"),
 };
 
-export const Square: Story = {
+export const Default: Story = {
   args: {
     image: {
       id: "1",
       url: "https://placehold.jp/300x300.png",
-      reported: false,
-    },
-    ...commonArgs,
-  },
-};
-
-export const Horizontal: Story = {
-  args: {
-    image: {
-      id: "1",
-      url: "https://placehold.jp/350x250.png",
-      reported: false,
-    },
-    ...commonArgs,
-  },
-};
-
-export const Vertical: Story = {
-  args: {
-    image: {
-      id: "1",
-      url: "https://placehold.jp/250x350.png",
-      reported: false,
-    },
-    ...commonArgs,
-  },
-};
-
-export const Mini: Story = {
-  args: {
-    image: {
-      id: "1",
-      url: "https://placehold.jp/200x200.png",
       reported: false,
     },
     ...commonArgs,
