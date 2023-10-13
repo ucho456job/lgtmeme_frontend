0a4a033be065f00629c581187465dd51f6b62b05
ucho456job <ucho456job@gmail.com>
2023-10-13 22:57:37 +0900

    Fix initialize docker

--- prisma/migrations/20231006114415_create_storage_policy/migration.sql
+++
@@ -1,7 +0,0 @@
--- Use Postgres to create a bucket.
-INSERT INTO storage.buckets (id, name, public) VALUES ('images', 'images', true);
-
--- Create storage policies
-CREATE POLICY "SELECT images" ON storage.objects FOR SELECT TO anon USING (bucket_id = 'images' AND auth.role() = 'anon');
-CREATE POLICY "INSERT images" ON storage.objects FOR INSERT TO anon WITH CHECK (bucket_id = 'images' AND auth.role() = 'anon');
-CREATE POLICY "DELETE images" ON storage.objects FOR DELETE TO anon USING (bucket_id = 'images' AND auth.role() = 'anon');
